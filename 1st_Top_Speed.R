9628ed110b6ac928ac414f139dbdc20cafefe9f1
sullyvan23 <stsullivan0605@gmail.com>
2025-07-23 10:41:55 -0400

    Create 1st_Top_Speed.R

---
+++ 1st_Top_Speed.R
@@ -0,0 +1,42 @@
+final_1st_stats <- final_1st_stats %>%
+    mutate(play_key = paste(game_str, play_id, sep = "_")) %>%
+    relocate(play_key, .after = play_id)
+
+run_max_1st <- player_pos %>%
+    # Create composite key
+    mutate(play_key = paste(game_str, play_id, sep = "_")) %>%
+    filter(
+        play_key %in% final_1st_stats$play_key,
+        player_position == 11
+    ) %>%
+    collect()
+
+run_max_1st <- run_max_1st %>%
+    mutate(time_key = paste(play_key, timestamp, sep="_"))
+lag_run_max1_1st <- run_max_1st[,1:11]
+
+lag_run_max1_1st <- lag_run_max1_1st %>%
+    mutate(time_key = paste(play_key, as.numeric(timestamp) + 500, sep="_"))
+
+lag_run_max2_1st <- run_max_1st[,1:11]
+
+lag_run_max2_1st <- lag_run_max2_1st %>%
+    mutate(time_key = paste(play_key, as.numeric(timestamp) + 495, sep="_"))
+
+lag_run_max_1st <- rbind(lag_run_max1_1st, lag_run_max2_1st)
+
+run_max_1st <- full_join(run_max_1st, lag_run_max_1st[,c(3,5:6,12)], by="time_key")
+
+run_max_1st <- run_max_1st %>%
+    mutate(max_speed = sqrt( (field_x.x - field_x.y)^2 + (field_y.x - field_y.y)^2 ) / ((timestamp.x - timestamp.y) /1000) )
+
+run_max_1st <- run_max_1st %>%
+    filter(!is.na(max_speed))
+
+run_top_speed_1st <- run_max_1st %>%
+    group_by(play_key) %>%
+    summarise(top_speed = max(max_speed), .groups = "drop") %>%
+    ungroup()
+
+final_1st_stats <- full_join(final_1st_stats, run_top_speed_1st, by="play_key") %>%
+    relocate(top_speed, .after = run_speed)
