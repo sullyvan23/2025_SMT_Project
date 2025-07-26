0e78ff5b3c978fce7154ca72e5cd664b977077a3
sullyvan23 <stsullivan0605@gmail.com>
2025-07-26 14:41:02 -0400

    Update 1st_Stay_Safe_Out.R

--- 1st_Stay_Safe_Out.R
+++ 1st_Stay_Safe_Out.R
@@ -1,6 +1,8 @@
 messy_stats_1st <- messy_stats_1st %>%
     mutate(play_key = paste(game_str, play_id, sep = "_"))
 
+
+# Getting how close the runner is to home at every point in play
 go_home_1st <- player_pos %>%
     # Create composite key
     mutate(play_key = paste(game_str, play_id, sep = "_")) %>%
@@ -15,17 +17,24 @@ go_home_1st <- player_pos %>%
 go_home_1st <- go_home_1st %>%
     mutate(home_dist = sqrt(field_x^2 + field_y^2) )
 
+
+# Seeing if the player went home based on their closest distance to home during the play
 go_home_1st <- go_home_1st %>%
     mutate(going = ifelse( home_dist <= 45, 1, 0) )
+
 close_home_1st <- go_home_1st %>%
     group_by(play_key) %>%
     slice_min(order_by = home_dist, n = 1, with_ties = FALSE) %>%
     ungroup()
 
+
+# Getting the timestamp at which they are closest to home
 close_home_1st <- close_home_1st %>% rename(run_home_time = timestamp)
 
 messy_stats_1st <- full_join(messy_stats_1st, close_home_1st[,c(3,7:9)], by = "play_key")
 
+
+# Seeing if and when the catcher acquired the ball during the play
 catcher_rec_1st <- game_events %>%
     # Create composite key
     mutate(play_key = paste(game_str, play_id, sep = "_")) %>%
@@ -39,18 +48,28 @@ catcher_rec_1st <- game_events %>%
     collect()
 
 catcher_rec_1st <- catcher_rec_1st %>% rename(catch_time = timestamp)
+
 messy_stats_1st <- full_join(messy_stats_1st, catcher_rec_1st[,c(3,6)], by = "play_key")
 
+
+# Comparing time the catcher acquired the ball and time the runner was closest to home
 messy_stats_1st <- messy_stats_1st %>%
     mutate(run_min_catch_time = (run_home_time - catch_time) / 1000)
 
 messy_stats_1st <- messy_stats_1st %>%
     mutate(catch_before_score = ifelse( run_min_catch_time > 0, 1, 0 ) )
+
+
+# Organzing needed data into a new dataframe
 stats_1st <- messy_stats_1st %>%
     dplyr::select(game_str, play_id, run_dist, OF_dist, run_speed, OF_momentum_home, OF_momentum_side, home_dist, going, run_min_catch_time, catch_before_score)
 
-# EXCEL WORK
 
+# Used the runner and catcher info here to determine whether there is a chance the runner was out, and animating those plays to determine the chance they were out, did this info in Excel
+
+final_1st_stats <- read_csv("final_1st_stats.csv")
+
+# Filtering out plays I saw that made them non applicable
 final_1st_stats <- final_1st_stats %>%
     filter(!is.na(score_chance))
 
