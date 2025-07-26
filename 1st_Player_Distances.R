9e690ad94a889f3df9cf780c2b9a7234787da68f
sullyvan23 <stsullivan0605@gmail.com>
2025-07-26 14:14:53 -0400

    Update 1st_Player_Distances.R

--- 1st_Player_Distances.R
+++ 1st_Player_Distances.R
@@ -1,10 +1,10 @@
 messy_stats_1st <- messy_stats_1st %>%
-    mutate(run_dist = ifelse( (run_x >= 0 & run_y > 90/sqrt(2)) , 
-        (180 + sqrt( (run_x)^2 + (run_y - (90*sqrt(2)))^2 ) ) ,
-        ifelse( (run_x < 0 & run_y > 90/sqrt(2)) , 
-            (90 + sqrt( (run_x + (90/sqrt(2)))^2 + (run_y - (90/sqrt(2)))^2 ) ) , 
-            ifelse( (run_x > 0 | run_y < 0), 0, sqrt(run_x^2 + run_y^2) ) ) 
+    mutate(run_dist = ifelse( (run_x >= 0 & run_y > 90/sqrt(2)) ,                         # if runner hasn't reached second base yet
+        (180 + sqrt( (run_x)^2 + (run_y - (90*sqrt(2)))^2 ) ) ,                           # distance to second base + 180    
+        ifelse( (run_x < 0 & run_y > 90/sqrt(2)) ,                                        # if runner hasn't reached third base yet
+            (90 + sqrt( (run_x + (90/sqrt(2)))^2 + (run_y - (90/sqrt(2)))^2 ) ) ,         # distance to third base + 90     
+            ifelse( (run_x > 0 | run_y < 0), 0, sqrt(run_x^2 + run_y^2) ) )               # else, distance to home
     ) )
 
 messy_stats_1st <- messy_stats_1st %>%
-    mutate(OF_dist = sqrt(OF_x^2 + OF_y^2))
+    mutate(OF_dist = sqrt(OF_x^2 + OF_y^2))                    # OF distance to home
