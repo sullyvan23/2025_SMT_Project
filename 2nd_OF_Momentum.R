f412943b00f3f48538821e5d0ee94af4d7e39d7a
sullyvan23 <stsullivan0605@gmail.com>
2025-07-26 14:24:03 -0400

    Update 2nd_OF_Momentum.R

--- 2nd_OF_Momentum.R
+++ 2nd_OF_Momentum.R
@@ -1,3 +1,4 @@
+# Getting position of outfielder ~0.5 seconds before they acquire the ball
 messy_stats <- messy_stats %>%
     mutate(lag_key_of = paste(game_str, play_id, lag_timestamp, player_position, sep = "_"))
 
@@ -14,14 +15,20 @@ run2nd_OF_lag_pos <- player_pos %>%
 run2nd_OF_lag_pos <- run2nd_OF_lag_pos %>% rename(lag_OF_x = field_x)
 run2nd_OF_lag_pos <- run2nd_OF_lag_pos %>% rename(lag_OF_y = field_y)
 
+
+# Join lag positions to messy stats where position at ball acquisition data is
 messy_stats <- full_join(messy_stats, run2nd_OF_lag_pos[,5:7], by = "lag_key_of")
 
+
+# Getting outfielders velocity in x and y directins in ft/sec
 messy_stats <- messy_stats %>%
     mutate(
         OF_x_velo = ( (OF_x - lag_OF_x) / ((timestamp - lag_timestamp)/1000) ),
         OF_y_velo = ( (OF_y - lag_OF_y) / ((timestamp - lag_timestamp)/1000) )
     )
 
+
+# Projection function to calculate speed in direction of home and side
 OF_proj <- function(x, y, a, b) {
     
     A <- c(x,y)
@@ -33,6 +40,8 @@ OF_proj <- function(x, y, a, b) {
     return(proj_A_on_B)
  }
 
+
+# Geting projection of speeds in x and y onto home and side vectors
 messy_stats <- messy_stats %>%
     rowwise() %>%
     mutate(
@@ -43,6 +52,7 @@ messy_stats <- messy_stats %>%
         OF_side_mom = list(OF_proj(OF_x_velo, OF_y_velo, -OF_y, OF_x))
     ) %>%
     ungroup()
+
 messy_stats <- messy_stats %>%
     mutate(
         OF_home_x = sapply(OF_home_mom, `[`, 1) ,
@@ -51,6 +61,8 @@ messy_stats <- messy_stats %>%
         OF_side_y = sapply(OF_side_mom, `[`, 2)   
     )
 
+
+# Getting home and side momentum (for home momentum, positive = going towards home, negative = away from home) (side momentum always positive)
 messy_stats <- messy_stats %>%
     mutate(
         OF_momentum_home = ifelse( OF_home_y >= 0,
