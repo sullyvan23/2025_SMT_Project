1c39e8a1dee8bd30f86fc48debbdb4b7351dae9a
sullyvan23 <stsullivan0605@gmail.com>
2025-07-26 14:18:11 -0400

    Update 2nd_Run_Speed.R

--- 2nd_Run_Speed.R
+++ 2nd_Run_Speed.R
@@ -1,3 +1,4 @@
+# Getting runner position ~0.5 seconds back (500 for plays where they go by 50 to 100, 495 for plays where they go by 33)
 run2nd_lag_pos <- player_pos %>%
     # Create composite key
     mutate(key_run = paste(game_str, play_id, timestamp + 500, sep = "_")) %>%
@@ -20,13 +21,19 @@ run2nd_lag_pos_2.0 <- player_pos %>%
     select(-c("Season", "HomeTeam", "AwayTeam", "Day")) %>%
     collect()
 
+
+# Combining lag positions
 run2nd_lag_pos <- rbind(run2nd_lag_pos_1.0, run2nd_lag_pos_2.0)
 
 run2nd_lag_pos <- run2nd_lag_pos %>% rename(lag_timestamp = timestamp)
 run2nd_lag_pos <- run2nd_lag_pos %>% rename(lag_run_x = field_x)
 run2nd_lag_pos <- run2nd_lag_pos %>% rename(lag_run_y = field_y)
 
+
+# Join to messy stats which has current position data
 messy_stats <- full_join(messy_stats, run2nd_lag_pos[,c(3,5:7)], by = "key_run")
 
+
+# Getting speed over those ~0.5 seconds in ft/sec
 messy_stats <- messy_stats %>%
     mutate( run_speed = ( sqrt((run_x - lag_run_x)^2 + (run_y - lag_run_y)^2) /  ((timestamp - lag_timestamp)/1000) ) )
