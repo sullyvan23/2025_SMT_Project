641c8114ffaae6b75ecc9c3029bde5c95fb95734
sullyvan23 <stsullivan0605@gmail.com>
2025-07-28 19:14:21 -0400

    Update GLM_GAM_Modeling.R

--- GLM_GAM_Modeling.R
+++ GLM_GAM_Modeling.R
@@ -1,16 +1,18 @@
+# Testing bunch of different combinations with logistic regression model
 ds_gam_results <- data.frame(test_num = integer(), logloss = numeric())
-
+# Doiung 100 tests for each model
 for (i in 1:100) {
-
+  # Creating 10 statified folds of went data based off def_safe
   strat_folds <- createFolds(all_went_data$def_safe, k = 10, list = TRUE, returnTrain = FALSE)
 
   tests_log_losses <- c()
 
+  # looping through all combinations of 9 train folds and 1 test fold
   for (fold in strat_folds) {
     test_data <- all_went_data[fold, ]
-
+    # Model used for test
     gam <- gam(def_safe ~ run_dist + OF_dist + run_speed + top_speed + OF_momentum_home + OF_momentum_side, data = all_went_data[-fold, ], family = binomial)
-
+    # Finding log loss between predicted probability and score_chance
     test_probs <- predict(gam, newdata = test_data, type = "response")
 
     fold_log_losses <- log_loss(test_data$score_chance, test_probs)
@@ -19,17 +21,20 @@ for (i in 1:100) {
     }
 
   avg_log_losses <- mean(tests_log_losses)
-
+  # Putting logloss of tests into a dataframe
   ds_gam_results <- bind_rows(ds_gam_results, data.frame(test_num = i, logloss = avg_log_losses))
 }
-
+# Finding mean and sd logloss on 100 tests
 mean(ds_gam_results$logloss)
 sd(ds_gam_results$logloss)
 
-> mean(ds_gam_results$logloss)
-[1] 0.1557032
-> sd(ds_gam_results$logloss)
-[1] 0.002372976
+# mean(ds_gam_results$logloss)
+# 0.1557032
+# sd(ds_gam_results$logloss)
+# 0.002372976
+
+################################################################################################################################################################
+
 ds_gam_results_2 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -58,10 +63,13 @@ for (i in 1:100) {
 mean(ds_gam_results_2$logloss)
 sd(ds_gam_results_2$logloss)
 
-> mean(ds_gam_results_2$logloss)
-[1] 0.1576292
-> sd(ds_gam_results_2$logloss)
-[1] 0.003358983
+# mean(ds_gam_results_2$logloss)
+# 0.1576292
+# sd(ds_gam_results_2$logloss)
+# 0.003358983
+
+################################################################################################################################################################
+
 ds_gam_results_3 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -90,10 +98,13 @@ for (i in 1:100) {
 mean(ds_gam_results_3$logloss)
 sd(ds_gam_results_3$logloss)
 
-> mean(ds_gam_results_3$logloss)
-[1] 0.1558588
-> sd(ds_gam_results_3$logloss)
-[1] 0.002302427
+# mean(ds_gam_results_3$logloss)
+# 0.1558588
+# sd(ds_gam_results_3$logloss)
+# 0.002302427
+
+################################################################################################################################################################
+
 ds_gam_results_4 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -122,10 +133,13 @@ for (i in 1:100) {
 mean(ds_gam_results_4$logloss)
 sd(ds_gam_results_4$logloss)
 
-> mean(ds_gam_results_4$logloss)
-[1] 0.1766857
-> sd(ds_gam_results_4$logloss)
-[1] 0.007814122
+# mean(ds_gam_results_4$logloss)
+# 0.1766857
+# sd(ds_gam_results_4$logloss)
+# 0.007814122
+
+################################################################################################################################################################
+
 ds_gam_results_5 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -154,10 +168,13 @@ for (i in 1:100) {
 mean(ds_gam_results_5$logloss)
 sd(ds_gam_results_5$logloss)
 
-> mean(ds_gam_results_5$logloss)
-[1] 0.1565778
-> sd(ds_gam_results_5$logloss)
-[1] 0.002314232
+# mean(ds_gam_results_5$logloss)
+# 0.1565778
+# sd(ds_gam_results_5$logloss)
+# 0.002314232
+
+################################################################################################################################################################
+
 ds_gam_results_6 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -186,10 +203,13 @@ for (i in 1:100) {
 mean(ds_gam_results_6$logloss)
 sd(ds_gam_results_6$logloss)
 
-> mean(ds_gam_results_6$logloss)
-[1] 0.1562374
-> sd(ds_gam_results_6$logloss)
-[1] 0.003356306
+# mean(ds_gam_results_6$logloss)
+# 0.1562374
+# sd(ds_gam_results_6$logloss)
+# 0.003356306
+
+################################################################################################################################################################
+
 ds_gam_results_7 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -218,10 +238,13 @@ for (i in 1:100) {
 mean(ds_gam_results_7$logloss)
 sd(ds_gam_results_7$logloss)
 
-> mean(ds_gam_results_7$logloss)
-[1] 0.1561013
-> sd(ds_gam_results_7$logloss)
-[1] 0.002446433
+# mean(ds_gam_results_7$logloss)
+# 0.1561013
+# sd(ds_gam_results_7$logloss)
+# 0.002446433
+
+################################################################################################################################################################
+
 ds_gam_results_8 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -250,10 +273,13 @@ for (i in 1:100) {
 mean(ds_gam_results_8$logloss)
 sd(ds_gam_results_8$logloss)
 
-> mean(ds_gam_results_8$logloss)
-[1] 0.1564697
-> sd(ds_gam_results_8$logloss)
-[1] 0.002149885
+# mean(ds_gam_results_8$logloss)
+# 0.1564697
+# sd(ds_gam_results_8$logloss)
+# 0.002149885
+
+################################################################################################################################################################
+
 ds_gam_results_9 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -282,10 +308,13 @@ for (i in 1:100) {
 mean(ds_gam_results_9$logloss)
 sd(ds_gam_results_9$logloss)
 
-> mean(ds_gam_results_9$logloss)
-[1] 0.1628725
-> sd(ds_gam_results_9$logloss)
-[1] 0.003755322
+# mean(ds_gam_results_9$logloss)
+# 0.1628725
+# sd(ds_gam_results_9$logloss)
+# 0.003755322
+
+################################################################################################################################################################
+
 ds_gam_results_10 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -314,10 +343,13 @@ for (i in 1:100) {
 mean(ds_gam_results_10$logloss)
 sd(ds_gam_results_10$logloss)
 
-> mean(ds_gam_results_10$logloss)
-[1] 0.1580067
-> sd(ds_gam_results_10$logloss)
-[1] 0.002773877
+# mean(ds_gam_results_10$logloss)
+# 0.1580067
+# sd(ds_gam_results_10$logloss)
+# 0.002773877
+
+################################################################################################################################################################
+
 ds_gam_results_11 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -346,10 +378,13 @@ for (i in 1:100) {
 mean(ds_gam_results_11$logloss)
 sd(ds_gam_results_11$logloss)
 
-> mean(ds_gam_results_11$logloss)
-[1] 0.1607214
-> sd(ds_gam_results_11$logloss)
-[1] 0.005421879
+# mean(ds_gam_results_11$logloss)
+# 0.1607214
+# sd(ds_gam_results_11$logloss)
+# 0.005421879
+
+################################################################################################################################################################
+
 ds_gam_results_12 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -378,10 +413,13 @@ for (i in 1:100) {
 mean(ds_gam_results_12$logloss)
 sd(ds_gam_results_12$logloss)
 
-> mean(ds_gam_results_12$logloss)
-[1] 0.1545407
-> sd(ds_gam_results_12$logloss)
-[1] 0.00234596
+# mean(ds_gam_results_12$logloss)
+# 0.1545407
+# sd(ds_gam_results_12$logloss)
+# 0.00234596
+
+################################################################################################################################################################
+
 ds_gam_results_13 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -410,10 +448,13 @@ for (i in 1:100) {
 mean(ds_gam_results_13$logloss)
 sd(ds_gam_results_13$logloss)
 
-> mean(ds_gam_results_13$logloss)
-[1] 0.1568676
-> sd(ds_gam_results_13$logloss)
-[1] 0.002627631
+# mean(ds_gam_results_13$logloss)
+# 0.1568676
+# sd(ds_gam_results_13$logloss)
+# 0.002627631
+
+################################################################################################################################################################
+
 ds_gam_results_14 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -442,10 +483,13 @@ for (i in 1:100) {
 mean(ds_gam_results_14$logloss)
 sd(ds_gam_results_14$logloss)
 
-> mean(ds_gam_results_14$logloss)
-[1] 0.1578575
-> sd(ds_gam_results_14$logloss)
-[1] 0.002249428
+# mean(ds_gam_results_14$logloss)
+# 0.1578575
+# sd(ds_gam_results_14$logloss)
+# 0.002249428
+
+################################################################################################################################################################
+
 ds_gam_results_15 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -474,10 +518,13 @@ for (i in 1:100) {
 mean(ds_gam_results_15$logloss)
 sd(ds_gam_results_15$logloss)
 
-> mean(ds_gam_results_15$logloss)
-[1] 0.1581677
-> sd(ds_gam_results_15$logloss)
-[1] 0.00253089
+# mean(ds_gam_results_15$logloss)
+# 0.1581677
+# sd(ds_gam_results_15$logloss)
+# 0.00253089
+
+################################################################################################################################################################
+
 ds_gam_results_16 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -506,10 +553,13 @@ for (i in 1:100) {
 mean(ds_gam_results_16$logloss)
 sd(ds_gam_results_16$logloss)
 
-> mean(ds_gam_results_16$logloss)
-[1] 0.1558783
-> sd(ds_gam_results_16$logloss)
-[1] 0.002284116
+# mean(ds_gam_results_16$logloss)
+# 0.1558783
+# sd(ds_gam_results_16$logloss)
+# 0.002284116
+
+################################################################################################################################################################
+
 ds_gam_results_17 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -538,10 +588,13 @@ for (i in 1:100) {
 mean(ds_gam_results_17$logloss)
 sd(ds_gam_results_17$logloss)
 
-> mean(ds_gam_results_17$logloss)
-[1] 0.1550032
-> sd(ds_gam_results_17$logloss)
-[1] 0.002426066
+# mean(ds_gam_results_17$logloss)
+# 0.1550032
+# sd(ds_gam_results_17$logloss)
+# 0.002426066
+
+################################################################################################################################################################
+
 ds_gam_results_18 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -570,10 +623,13 @@ for (i in 1:100) {
 mean(ds_gam_results_18$logloss)
 sd(ds_gam_results_18$logloss)
 
-> mean(ds_gam_results_18$logloss)
-[1] 0.1525796
-> sd(ds_gam_results_18$logloss)
-[1] 0.002865385
+# mean(ds_gam_results_18$logloss)
+# 0.1525796
+# sd(ds_gam_results_18$logloss)
+# 0.002865385
+
+################################################################################################################################################################
+
 ds_gam_results_19 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -602,10 +658,13 @@ for (i in 1:100) {
 mean(ds_gam_results_19$logloss)
 sd(ds_gam_results_19$logloss)
 
-> mean(ds_gam_results_19$logloss)
-[1] 0.1727093
-> sd(ds_gam_results_19$logloss)
-[1] 0.006809436
+# mean(ds_gam_results_19$logloss)
+# 0.1727093
+# sd(ds_gam_results_19$logloss)
+# 0.006809436
+
+################################################################################################################################################################
+
 ds_gam_results_20 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -634,10 +693,13 @@ for (i in 1:100) {
 mean(ds_gam_results_20$logloss)
 sd(ds_gam_results_20$logloss)
 
-> mean(ds_gam_results_21$logloss)
-[1] 0.1601373
-> sd(ds_gam_results_21$logloss)
-[1] 0.002646925
+# mean(ds_gam_results_21$logloss)
+# 0.1601373
+# sd(ds_gam_results_21$logloss)
+# 0.002646925
+
+################################################################################################################################################################
+
 ds_gam_results_21 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -666,10 +728,13 @@ for (i in 1:100) {
 mean(ds_gam_results_21$logloss)
 sd(ds_gam_results_21$logloss)
 
-> mean(ds_gam_results_21$logloss)
-[1] 0.1601373
-> sd(ds_gam_results_21$logloss)
-[1] 0.002646925
+# mean(ds_gam_results_21$logloss)
+# 0.1601373
+# sd(ds_gam_results_21$logloss)
+# 0.002646925
+
+################################################################################################################################################################
+
 ds_gam_results_22 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -698,10 +763,13 @@ for (i in 1:100) {
 mean(ds_gam_results_22$logloss)
 sd(ds_gam_results_22$logloss)
 
-> mean(ds_gam_results_22$logloss)
-[1] 0.161732
-> sd(ds_gam_results_22$logloss)
-[1] 0.004687543
+# mean(ds_gam_results_22$logloss)
+# 0.161732
+# sd(ds_gam_results_22$logloss)
+# 0.004687543
+
+################################################################################################################################################################
+
 ds_gam_results_23 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -730,10 +798,13 @@ for (i in 1:100) {
 mean(ds_gam_results_23$logloss)
 sd(ds_gam_results_23$logloss)
 
-> mean(ds_gam_results_23$logloss)
-[1] 0.1576742
-> sd(ds_gam_results_23$logloss)
-[1] 0.004162598
+# mean(ds_gam_results_23$logloss)
+# 0.1576742
+# sd(ds_gam_results_23$logloss)
+# 0.004162598
+
+################################################################################################################################################################
+
 ds_gam_results_24 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -762,10 +833,13 @@ for (i in 1:100) {
 mean(ds_gam_results_24$logloss)
 sd(ds_gam_results_24$logloss)
 
-> mean(ds_gam_results_24$logloss)
-[1] 0.1732043
-> sd(ds_gam_results_24$logloss)
-[1] 0.008872286
+# mean(ds_gam_results_24$logloss)
+# 0.1732043
+# sd(ds_gam_results_24$logloss)
+# 0.008872286
+
+################################################################################################################################################################
+
 ds_gam_results_25 <- data.frame(test_num = integer(), logloss = numeric())
 
 for (i in 1:100) {
@@ -794,10 +868,13 @@ for (i in 1:100) {
 mean(ds_gam_results_25$logloss)
 sd(ds_gam_results_25$logloss)
 
-> mean(ds_gam_results_25$logloss)
-[1] 0.1624453
-> sd(ds_gam_results_25$logloss)
-[1] 0.00506908
+# mean(ds_gam_results_25$logloss)
+# 0.1624453
+# sd(ds_gam_results_25$logloss)
+# 0.00506908
+
+################################################################################################################################################################
+# Tried something with combining run_speed and top_speed into one stat, didn't really do anything
 pca_speed <- prcomp(all_went_data[, c("run_speed", "top_speed")], scale. = TRUE)
 all_went_data$pca_speed <- pca_speed$x[, 1]
 
@@ -826,7 +903,7 @@ for (i in 1:100) {
   ds_gam_results_26 <- bind_rows(ds_gam_results_26, data.frame(test_num = i, logloss = avg_log_losses))
 }
 
-> mean(ds_gam_results_26$logloss)
-[1] 0.1555125
-> sd(ds_gam_results_26$logloss)
-[1] 0.003159275
+# mean(ds_gam_results_26$logloss)
+# 0.1555125
+# sd(ds_gam_results_26$logloss)
+# 0.003159275
