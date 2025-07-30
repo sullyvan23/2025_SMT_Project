743780a2fd974606a6028fb226b2631d4c2f8699
sullyvan23 <stsullivan0605@gmail.com>
2025-07-29 21:44:57 -0400

    Update 1st_Player_Distances.R

--- 1st_Player_Distances.R
+++ 1st_Player_Distances.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 messy_stats_1st <- messy_stats_1st %>%
     mutate(run_dist = ifelse( (run_x >= 0 & run_y > 90/sqrt(2)) ,                         # if runner hasn't reached second base yet
         (180 + sqrt( (run_x)^2 + (run_y - (90*sqrt(2)))^2 ) ) ,                           # distance to second base + 180    
